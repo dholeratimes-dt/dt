69e7cf9220dae219c3c52c0a4a642d9ddd118335
Naman <namangupta2360@gmail.com>
2026-04-22 12:26:42 +0530

    changes

--- src/app/(main)/dholera-updates/latest-updates/[slug]/page.js
+++ src/app/(main)/dholera-updates/latest-updates/[slug]/page.js
@@ -702,7 +702,7 @@ export default async function BlogDetail({ params }) {
                           d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                         ></path>
                       </svg>
-                      <time className="text-gray-500">{formattedDate}</time>
+                     <time className="text-gray-500" dateTime={isoDate}>{formattedDate}</time>
                     </div>
 
                     {post.readingTime && (
