534cc388d3237e6862f20195887b4b8e85d4453b
tdd-dholera <topdealsdholera@gmail.com>
2026-05-20 14:11:09 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: 'standalone',
+   poweredByHeader: false, 
   images: {
     domains: ["cdn.sanity.io"], // Allow Sanity images
   },
