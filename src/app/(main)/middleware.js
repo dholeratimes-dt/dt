9e3186bf2f92793b2f254835c18f10e13e8cefdd
tdd-dholera <topdealsdholera@gmail.com>
2026-05-29 13:10:14 +0530

    changes

--- src/app/(main)/middleware.js
+++ src/app/(main)/middleware.js
@@ -9,7 +9,6 @@ const client = createClient({
   useCdn: false,
 })
 
-
 const SITE_NAME = process.env.SITE_NAME || 'dholera-times'
 const SITE_URL = process.env.SITE_URL || 'https://www.dholeratimes.com'
 
